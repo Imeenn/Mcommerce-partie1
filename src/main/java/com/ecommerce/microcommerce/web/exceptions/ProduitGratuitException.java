ac1b06f194c55a7d136bc79707a98df4e179b8a1
Imeenn <imen.mechergui1@gmail.com>
2019-09-10 21:26:16 +0200

    exception produit vide

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -2,7 +2,7 @@ package com.ecommerce.microcommerce.web.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+//classe d exception de produit gratuit c-a-d prix=0
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProduitGratuitException extends RuntimeException {
 	
